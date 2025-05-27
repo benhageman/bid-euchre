656a11fc748fd4f4a05ffa642ddf7a54066ea614
Nick Hageman <nickhageman8@gmail.com>
2025-05-27 11:03:51 -0500

    Switching from localhost to Render-hosted server

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 // src/socket.ts
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:3001', {
+export const socket = io('wss://bid-euchre.onrender.com', {
   autoConnect: true,
 });
